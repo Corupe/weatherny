5e806fabafc7f1b99d36f93a6f6c56b793977bb6
corupe <mezioud.kecha@gmail.com>
2024-11-06 11:47:59 +0100

    weatherny

--- src/hooks/useGetWeather.ts
+++ src/hooks/useGetWeather.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 // const weatherAPI = "http://api.weatherapi.com/v1/current.json?key=cdfac984ac23407eb34143813240311&q=algeria&aqi=no";
-const weatherAPI = "http://api.weatherapi.com/v1/current.json?key=cdfac984ac23407eb34143813240311";
+const weatherAPI = "https://api.weatherapi.com/v1/current.json?key=cdfac984ac23407eb34143813240311";
 
 export const useGetWeather = (city: string) => {
 	const { data, status } = useQuery<any, Error>({
